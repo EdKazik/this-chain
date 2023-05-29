12d0ed50ef6c7c7edc303cec0a2858e7881060c9
EdKazik <edoskazik@gmail.com>
2023-05-30 00:19:52 +0200

    commit 2 task9

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let ladder = {
     step: 0,
     up: function () {
         this.step++;
-        return ladder
+        return this
     },
     down: function () {
         this.step--;
-        return ladder
+        return this
     },
     showStep: function () { 
         alert(this.step);
